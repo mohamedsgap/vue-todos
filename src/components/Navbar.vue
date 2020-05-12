759d6c7ba0da9956e7605249b0ceaf6279eb61ae
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-05-13 00:17:40 +0200

    Update Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,58 @@
 <template>
-  <div></div>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="">Vue Todos</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item mt-1">
+          <router-link :to="{ name: 'Todos' }" class="nav-link p-3">
+            Todos
+          </router-link>
+        </li>
+        <li class="nav-item mt-1">
+          <router-link :to="{ name: 'CompletedTodos' }" class="nav-link p-3">
+            Completed Todos
+          </router-link>
+        </li>
+        <li class="nav-item mt-1">
+          <router-link :to="{ name: 'About' }" class="nav-link p-3">
+            About
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "NavBar",
+  name: "Navbar",
   data() {
     return {};
   }
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.navbar {
+  background-color: #37db60;
+}
+.nav-item .nav-link {
+  color: azure;
+  font-size: 25px;
+}
+.navbar-light .navbar-brand {
+  color: azure;
+  font-size: 35px;
+}
+</style>

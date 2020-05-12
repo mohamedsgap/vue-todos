e30700b5eb26724229042304420c2915f3101343
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-05-13 01:51:12 +0200

    Feat: filter todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,14 +5,16 @@
         <h1>{{ listName }}</h1>
       </div>
     </div>
-    <div class="row mb-3">
+
+    <div class="row mb-3" v-if="todosCategory != 'done'">
       <create-todo @on-new-todo="addTodo($event)" />
     </div>
+
     <div class="row">
       <div class="col-12 col-sm-10 col-lg-6">
         <ul class="list-group">
           <todo
-            v-for="(todo, index) in todos"
+            v-for="(todo, index) in showTodos"
             :key="index"
             :description="todo.description"
             :completed="todo.completed"
@@ -32,7 +34,14 @@ import CreateTodo from "./CreateTodo.vue";
 
 export default {
   props: {
-    listName: String
+    listName: String,
+    todosCategory: {
+      type: String,
+      default: "ALL_TODOS",
+      filterTodo: index => {
+        return ["COMPLETED_TODOS", "ALL_TODOS"].indexOf(index) !== -1;
+      }
+    }
   },
   data() {
     return {
@@ -44,6 +53,15 @@ export default {
     const todos = JSON.parse(localStorage.getItem("vue-todos"));
     if (todos) {
       this.todos = todos;
+    }
+  },
+  computed: {
+    showTodos() {
+      if (this.todosCategory === "COMPLETED_TODOS") {
+        return this.todos.filter(todo => todo.completed);
+      } else {
+        return this.todos;
+      }
     }
   },
   methods: {
